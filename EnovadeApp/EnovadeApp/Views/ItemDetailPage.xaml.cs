06d5645bd9754bbe41d2400d777f71634aacf3b4
Cloud Connect <Cloud Connect@CLOUDCONNECT-OM>
2022-04-25 17:33:55 +0800

    Add project files.

---
+++ EnovadeApp/EnovadeApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using EnovadeApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace EnovadeApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ EnovadeApp/EnovadeApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using EnovadeApp.Models;
+using EnovadeApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EnovadeApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
